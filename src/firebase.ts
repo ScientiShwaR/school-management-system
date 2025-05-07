7ba3ec5cc596eb7a8ac444c509baaff019ec3355
iShwaR ShaRma <sharmaishar@gmail.com>
2025-05-08 03:33:57 +0530

    Added FireStore and FireStorage

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,23 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// school-management-system
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDNE-jQvTKlISO9cOC4YYhoHYSaFs-OJZE",
+//   authDomain: "school-management-system-19dc5.firebaseapp.com",
+//   projectId: "school-management-system-19dc5",
+//   storageBucket: "school-management-system-19dc5.firebasestorage.app",
+//   messagingSenderId: "837875657790",
+//   appId: "1:837875657790:web:6150738e9e02af479db56f"
+// };
+// vue-firebase-auth - TEMP
 const firebaseConfig = {
-  apiKey: "AIzaSyDNE-jQvTKlISO9cOC4YYhoHYSaFs-OJZE",
-  authDomain: "school-management-system-19dc5.firebaseapp.com",
-  projectId: "school-management-system-19dc5",
-  storageBucket: "school-management-system-19dc5.firebasestorage.app",
-  messagingSenderId: "837875657790",
-  appId: "1:837875657790:web:6150738e9e02af479db56f"
+  apiKey: "AIzaSyAPSnvQHbq2UsgnhLBosvT__RMZlqOjKcw",
+  authDomain: "vue-firebase-auth-66525.firebaseapp.com",
+  projectId: "vue-firebase-auth-66525",
+  storageBucket: "vue-firebase-auth-66525.firebasestorage.app",
+  messagingSenderId: "262120530166",
+  appId: "1:262120530166:web:6b22e8680a3ee412868cd7"
 };
 
 const app = initializeApp(firebaseConfig);
